0d4053d19a0049a90417438e225d5777fa49152c
Rafiw Idrissu <86615946+Idrissu-Rafiu@users.noreply.github.com>
2022-12-20 11:53:58 -0800

    first commit
    design the UI

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".form");
+const formControl = document.querySelector(".form-control");
+const table = document.querySelector(".table");
+const tableBody = document.querySelector(".table-body");
+const getDataBtn = document.querySelector(".btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { value } = formControl;
+});

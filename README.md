d7e3b5f1b02775d3aef92f2419f18d3fda99632d
Rafiw Idrissu <86615946+Idrissu-Rafiu@users.noreply.github.com>
2022-12-27 16:22:37 -0800

    update
    add api and styling of input form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # First Commit
+
+A simple app that uses a natural language api to extract nutrition data from a text.
--- script.js
+++ script.js
@@ -1,10 +1,63 @@
+const tableOverlay = document.querySelector(".table-overlay");
+const closeBtn = document.querySelector(".close-btn");
 const form = document.querySelector(".form");
 const formControl = document.querySelector(".form-control");
 const table = document.querySelector(".table");
 const tableBody = document.querySelector(".table-body");
 const getDataBtn = document.querySelector(".btn");
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "38dd258aebmshec9cdf190c739c7p13ae03jsne258e0911332",
+    "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+  },
+};
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const { value } = formControl;
+  if (value) {
+    getDataBtn.innerText = "Loading...";
+    getData(value);
+  }
+  formControl.value = "";
 });
+
+async function getData(query) {
+  try {
+    const response = await fetch(
+      `https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${query}`,
+      options
+    );
+    const data = await response.json();
+    getDataBtn.innerText = "Get Data";
+    if (data.items[0] === undefined) {
+      return;
+    } else {
+      displayData(data.items[0]);
+      tableOverlay.style.display = "block";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+closeBtn.addEventListener("click", () => {
+  tableOverlay.style.display = "none";
+});
+
+function displayData(obj) {
+  tableBody.innerHTML = "";
+  for (const prop in obj) {
+    const tableRow = document.createElement("tr");
+    const nutrientData = document.createElement("td");
+    const amountData = document.createElement("td");
+    nutrientData.innerText = `${prop}`;
+    amountData.innerText = `${obj[prop]}`;
+    tableRow.append(nutrientData, amountData);
+    tableBody.appendChild(tableRow);
+  }
+}
+
+function showAlert(message, type) {}
